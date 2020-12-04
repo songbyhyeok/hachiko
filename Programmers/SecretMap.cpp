50e38295bad40147b9a4cfd7a8166dad6b5b34ba
songbyhyeok <songbyhyeok@naver.com>
2020-12-04 22:47:34 +0900

    12-04

--- Programmers/SecretMap.cpp
+++ Programmers/SecretMap.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void solution(int n, vector<int> arr1, vector<int> arr2) 
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2)
 {
 	vector<vector<vector<int>>> binaryVec3(2, vector<vector<int>>(n, vector<int>(n, 0)));
 
@@ -12,29 +12,40 @@ void solution(int n, vector<int> arr1, vector<int> arr2)
 	while(mapIdx < binaryVec3.size())
 	{
 		vector<int> currMap = mapIdx == 0 ? arr1 : arr2;
-		int  mapPos = 0, currPos = 0, binaryPos = 0;
+		int mapPos = 0, currPos = 0, binaryPos = 0;
 
 		while (mapPos < currMap.size())
 		{
-			binaryVec3[mapIdx][mapPos][binaryPos] = currMap[currPos] % 2;
-			binaryPos != 4 ? binaryPos++ : binaryPos = 0;
-			currMap[currPos] != 0 ? (currMap[currPos] /= 2) : currPos++;
-			mapPos = currPos < n ? mapPos : mapPos++;
+			if (currMap[currPos] != 0)
+			{
+				binaryVec3[mapIdx][mapPos][binaryPos++] = currMap[currPos] % 2;
+				currMap[currPos] /= 2;
+			}
+
+			else
+			{
+				binaryPos = 0;
+				currPos++;
+				mapPos++;
+			}
 		}
 
 		mapIdx++;
 	} 
-	
-	/*for (int i = binaryVec.size() - 1; i >= 0; i--)
-		for (int j = 0; j < binaryVec[i].size(); j++)
-		{
-			cout << binaryVec[i][j] << ' ';
-		}
-
-	cout << endl;*/
 
 	vector<string> answer;
-	//return answer;
+
+	for (int i = 0; i < n; i++)
+	{
+		string putChar;
+
+		for (int j = n - 1; j >= 0; j--)
+			binaryVec3[0][i][j] + binaryVec3[1][i][j] != 0 ? putChar += '#' : putChar += ' ';
+
+		answer.push_back(putChar);
+	}
+
+	return answer;
 }
 
 
@@ -43,18 +54,16 @@ int main()
 	// Case.1
 	int n = 5;
 	vector<int> arr1 = { 9, 20, 28, 18, 11 }, arr2 = { 30, 1, 21, 17, 28 };
-	/*for (int i = 0; i < n; i++)
-		cout << solution(n, arr1, arr2)[i] << ' ';
-	cout << endl;*/
 
 	// Case.2
 	int n2 = 6;
 	vector<int> arr3 = { 46, 33, 33, 22, 31, 50 }, arr4 = { 27, 56, 19, 14, 14, 10 };
-	/*for (int i = 0; i < n2; i++)
-		cout << solution(n2, arr3, arr4)[i] << ' ';*/
 
-	solution(n, arr1, arr2);
-	solution(n2, arr3, arr4);
+	vector<string> tempVec = solution(n, arr1, arr2);
+	vector<string> tempVec2 = solution(n2, arr3, arr4);
+
+	for (int i = 0; i < tempVec.size(); i++)
+		cout << tempVec[i] << endl;
 
 	return 0;
 }
